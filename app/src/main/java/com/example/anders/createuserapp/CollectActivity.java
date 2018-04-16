7677a8c7d30b4b4d00ba99a058b544836d35cd19
TheHansinator <38115936+TheHansinator@users.noreply.github.com>
2018-04-16 14:29:38 +0200

    Select artwork til indtastning af kode

--- app/src/main/java/com/example/anders/createuserapp/CollectActivity.java
+++ app/src/main/java/com/example/anders/createuserapp/CollectActivity.java
@@ -1,14 +1,14 @@
 package com.example.anders.createuserapp;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,8 +19,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
-
 public class CollectActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "CollectActivity";
 
@@ -30,6 +28,9 @@ public class CollectActivity extends AppCompatActivity implements View.OnClickLi
     private FirebaseAuth.AuthStateListener mAuthListener;
     private String userID;
     Button submitBtn;
+    Button btn_artwork;
+    Button btn_artwork2;
+
 
     EditText inputAnswer;
 
@@ -49,6 +50,12 @@ public class CollectActivity extends AppCompatActivity implements View.OnClickLi
         inputAnswer = (EditText) findViewById(R.id.textViewArtworkCode);
         findViewById(R.id.btn_addCode).setOnClickListener(this);
         submitBtn = (Button) findViewById(R.id.btn_addCode);
+        btn_artwork = (Button) findViewById(R.id.btn_artwork1);
+        btn_artwork2 = (Button) findViewById(R.id.btn_artwork2);
+
+
+
+
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -82,26 +89,72 @@ public class CollectActivity extends AppCompatActivity implements View.OnClickLi
 
     }
 
-    private void onData(DataSnapshot dataSnapshot) {
+    protected void onData(DataSnapshot dataSnapshot) {
             final String passcode1 = inputAnswer.getText().toString();
-            String artwork = (String) dataSnapshot.child("artworkss").child("artwork1").child("code").getValue();
-            if (passcode1.equals(artwork)) {
-                //do something
-                Toast.makeText(getApplicationContext(),"Nice",Toast.LENGTH_LONG).show();
+            String artwork = (String) dataSnapshot.child("artworkss").child(""+artwork_string).child("code").getValue();
+                if (passcode1.equals(artwork)) {
+                    //do something
+                    Toast.makeText(getApplicationContext(), "Nice", Toast.LENGTH_LONG).show();
 
 
-            } else if (!passcode1.equals(artwork)) {
-                Toast.makeText(getApplicationContext(),"Not so nice",Toast.LENGTH_LONG).show();
+                } else if (!passcode1.equals(artwork)) {
+                    Toast.makeText(getApplicationContext(), "Not so nice", Toast.LENGTH_LONG).show();
 
+                }
             }
-        }
+
+
 
     @Override
     public void onClick(View view) {
 
 
     }
-}
+
+    boolean identifier = false;
+    String artwork_string = "";
+
+    public void artwork1Select(View view) {
+        selecting_artwork1();
+    }
+
+
+        public void artwork2Select(View view) {
+        selecting_artwork2();
+            }
+
+    public void selecting_artwork1() {
+        if(!identifier){
+            artwork_string = "artwork1";
+            Toast.makeText(getApplicationContext(), "Artwork1 selected", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "Der sker ikke noget", Toast.LENGTH_LONG).show();
+            artwork_string = "";
+        }
+
+    }
+
+    public void selecting_artwork2() {
+        if(!identifier){
+            artwork_string = "artwork2";
+            Toast.makeText(getApplicationContext(), "Artwork2 selected", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "Der sker ikke noget", Toast.LENGTH_LONG).show();
+            artwork_string = "";
+        }
+
+    }
+
+
+
+    public void Display_selectedartwork(View view) {
+        }
+
+    }
+
+
+
+
 
 
 

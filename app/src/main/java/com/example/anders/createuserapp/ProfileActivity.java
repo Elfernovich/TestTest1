c0de46c34d6ef17b22dc48f2f5b5bbc276ae7c0e
Elfernovich <elfernovich@hotmail.com>
2018-05-03 09:13:53 +0200

    Bottom Navigation Bar

--- app/src/main/java/com/example/anders/createuserapp/ProfileActivity.java
+++ app/src/main/java/com/example/anders/createuserapp/ProfileActivity.java
@@ -1,13 +1,17 @@
 package com.example.anders.createuserapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +21,8 @@ public class ProfileActivity extends AppCompatActivity{
 
     private FirebaseAuth mAuth;
     TextView email;
+    ProgressBar prgBar;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +35,44 @@ public class ProfileActivity extends AppCompatActivity{
 
         email = (TextView) findViewById(R.id.textViewEmail);
 
+        prgBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
         loadUserInformation();
+        prgBar.setProgress(80);
 
 
 
-    }
+            //BottomNavigation Bar
+            bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+            Menu menu = bottomNavigationView.getMenu();
+            MenuItem menuItem = menu.getItem(0);
+            menuItem.setChecked(true);
+
+            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()){
+                        case R.id.id_profile:
+                            Intent intent1 = new Intent(ProfileActivity.this, ProfileActivity.class);
+                            intent1.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                            startActivity(intent1);
+                            //activity.startActivity(new Intent(activity, ProfileActivity.class));
+                            break;
+
+                        case R.id.id_collect:
+                            Intent intent3 = new Intent (ProfileActivity.this, CollectOverviewActivity.class);
+                            intent3.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                            startActivity(intent3);
+                            //mIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                            break;
+                    }
+                    return false;
+                }
+            });
+        }
+
+
 
 
     //If the user is not logged in he will be directed to the MainActivity.class
@@ -45,7 +84,6 @@ public class ProfileActivity extends AppCompatActivity{
             finish();
             startActivity(new Intent(this, MainActivity.class));
         }
-        UtilityBuilders.buildBottomNavigationView(this);
     }
 
     @Override
@@ -84,6 +122,9 @@ public class ProfileActivity extends AppCompatActivity{
         email.setText("Du er logget ind som:\n" + displayEmail);
     }
 
+    public void setProgressBar(){
+        setProgress(80);
+    }
 
     public void ButtonCollectActivity (View view) {
             finish();
